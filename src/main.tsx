d492ef5d950c16732b1d7ff885f491fb7a2ac3fe
Thach Anh Tran <myquartz@users.noreply.github.com>
2024-08-02 09:24:16 +0000

    Fix authentication provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,15 @@
-import React from "react";
+//import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
+import { Authenticator } from '@aws-amplify/ui-react';
 
 Amplify.configure(outputs);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
+  <Authenticator.Provider>
     <App />
-  </React.StrictMode>
+  </Authenticator.Provider>
 );
