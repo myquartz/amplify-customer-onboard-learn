6ea684e04bbf778d76e6ab7bf2dd1bba9a9f30b7
Thach Anh Tran <myquartz@users.noreply.github.com>
2024-08-07 10:27:37 +0000

    Sua lai ham increase

--- amplify/data/increase-sequence.js
+++ amplify/data/increase-sequence.js
@@ -1,20 +1,12 @@
 import { util } from '@aws-appsync/utils';
+import * as ddb from '@aws-appsync/utils/dynamodb';
 
 export function request(ctx) {
-  return {
-    operation: 'UpdateItem',
-    key: util.dynamodb.toMapValues({ seqKey: "customerSeq"}),
-    update: {
-      expression: 'SET #cnt = #cnt + :val, #time = :now',
-      expressionNames: {'#cnt': 'lastCifNumber','#time':'lastUpdateTime'},
-      expressionValues: { ':val': { 
-            "N": "1"
-        }, ':now': {
-            "S": util.time.nowISO8601()
-        } },
-      returnValues: "UPDATED_NEW"
-    }
-  }
+    const { seqKey, ...values } = ctx.args.input;
+	values.lastCifNumber = ddb.operations.increment(1);
+    values.lastUpdateTime = util.time.nowISO8601();
+	const condition = { seqKey: { attributeExists: true } };
+	return ddb.update({ key: { "seqKey":"customerSEQ" }, update: values, condition });
 }
 
 export function response(ctx) {
