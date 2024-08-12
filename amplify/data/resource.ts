bdb76247f81776c0a5b05c9f00164545651bcf79
Thach Anh Tran <myquartz@users.noreply.github.com>
2024-08-12 10:46:05 +0000

    Fix warning

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,6 +1,6 @@
 import { type ClientSchema, a, defineData,  } from "@aws-amplify/backend";
 
-import { checkIfAnAdmin, selfOnboarding } from "../functions/resource"
+import { checkIfAnAdmin } from "../functions/resource"
 
 /*== STEP 1 ===============================================================
 The section below creates a Todo database table with a "content" field. Try
